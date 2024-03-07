12073d5c41593f066d6327e2fa34a6b097749247
Jonathan Vuillemin <ekkinox@gmail.com>
2024-03-07 12:22:08 +0100

    feat(main): gRPC demo

--- grpc-demo/internal/services.go
+++ grpc-demo/internal/services.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"github.com/ankorstore/yokai-showroom/grpc-demo/internal/interceptor"
 	"github.com/ankorstore/yokai-showroom/grpc-demo/internal/service"
 	"github.com/ankorstore/yokai-showroom/grpc-demo/proto"
 	"github.com/ankorstore/yokai/fxgrpcserver"
@@ -10,6 +11,10 @@ import (
 // ProvideServices is used to register the application services.
 func ProvideServices() fx.Option {
 	return fx.Options(
-		fxgrpcserver.AsGrpcService(service.NewTransformTextServiceService, &proto.TransformTextService_ServiceDesc),
+		// interceptors
+		fxgrpcserver.AsGrpcServerUnaryInterceptor(interceptor.NewAuthenticationUnaryInterceptor),
+		fxgrpcserver.AsGrpcServerStreamInterceptor(interceptor.NewAuthenticationStreamInterceptor),
+		// service
+		fxgrpcserver.AsGrpcServerService(service.NewTransformTextServiceService, &proto.TransformTextService_ServiceDesc),
 	)
 }
