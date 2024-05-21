97a7fc6ee8fd5c92b28349adecf76d89a8ef3cd0
Jonathan Vuillemin <ekkinox@gmail.com>
2024-05-21 17:36:34 +0200

    feat: sql rework

--- http-demo/internal/service/gopher_test.go
+++ http-demo/internal/service/gopher_test.go
@@ -1,11 +1,220 @@
 package service_test
 
 import (
+	"context"
+	"database/sql"
+	"github.com/ankorstore/yokai-showroom/http-demo/internal"
+	"github.com/ankorstore/yokai-showroom/http-demo/internal/service"
+	"github.com/ankorstore/yokai/fxsql"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"go.uber.org/fx"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGet(t *testing.T) {
+	var db *sql.DB
+	var gopherService *service.GopherService
+	var metricsRegistry *prometheus.Registry
+
+	t.Run("should succeed", func(t *testing.T) {
+		// reset
+		service.GopherServiceCounter.Reset()
+
+		// run test
+		internal.RunTest(
+			t,
+			fxsql.RunFxSQLSeeds(),
+			fx.Populate(&db, &gopherService, &metricsRegistry),
+		)
+
+		// result assertion
+		gopher, err := gopherService.Get(context.Background(), 1)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "alice", gopher.Name)
+		assert.Equal(t, "frontend", gopher.Job.String)
+
+		// metrics assertion
+		expectedMetric := `
+			# HELP gophers_service_operations_total Number of operations on the GopherService
+			# TYPE gophers_service_operations_total counter
+			gophers_service_operations_total{operation="get"} 1
+		`
+
+		err = testutil.GatherAndCompare(
+			metricsRegistry,
+			strings.NewReader(expectedMetric),
+			"gophers_service_operations_total",
+		)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should fail", func(t *testing.T) {
+		// run test
+		internal.RunTest(t, fx.Populate(&db, &gopherService))
+
+		// close db
+		err := db.Close()
+		assert.NoError(t, err)
+
+		// result assertion
+		_, err = gopherService.Get(context.Background(), 1)
+		assert.Error(t, err)
+	})
+}
+
 func TestList(t *testing.T) {
-	assert.True(t, true)
+	var db *sql.DB
+	var gopherService *service.GopherService
+	var metricsRegistry *prometheus.Registry
+
+	t.Run("should succeed", func(t *testing.T) {
+		// reset
+		service.GopherServiceCounter.Reset()
+
+		// run test
+		internal.RunTest(
+			t,
+			fxsql.RunFxSQLSeeds(),
+			fx.Populate(&db, &gopherService, &metricsRegistry),
+		)
+
+		// result assertion
+		gophers, err := gopherService.List(context.Background(), "alice", "frontend")
+		assert.NoError(t, err)
+
+		assert.Len(t, gophers, 1)
+		assert.Equal(t, "alice", gophers[0].Name)
+		assert.Equal(t, "frontend", gophers[0].Job.String)
+
+		// metrics assertion
+		expectedMetric := `
+			# HELP gophers_service_operations_total Number of operations on the GopherService
+			# TYPE gophers_service_operations_total counter
+			gophers_service_operations_total{operation="list"} 1
+		`
+
+		err = testutil.GatherAndCompare(
+			metricsRegistry,
+			strings.NewReader(expectedMetric),
+			"gophers_service_operations_total",
+		)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should fail", func(t *testing.T) {
+		// run test
+		internal.RunTest(t, fx.Populate(&db, &gopherService))
+
+		// close db
+		err := db.Close()
+		assert.NoError(t, err)
+
+		// result assertion
+		_, err = gopherService.List(context.Background(), "", "")
+		assert.Error(t, err)
+	})
+}
+
+func TestCreate(t *testing.T) {
+	var db *sql.DB
+	var gopherService *service.GopherService
+	var metricsRegistry *prometheus.Registry
+
+	t.Run("should succeed", func(t *testing.T) {
+		// reset
+		service.GopherServiceCounter.Reset()
+
+		// run test
+		internal.RunTest(
+			t,
+			fx.Populate(&db, &gopherService, &metricsRegistry),
+		)
+
+		// result assertion
+		gopherId, err := gopherService.Create(context.Background(), "alice", "frontend")
+		assert.NoError(t, err)
+
+		assert.Equal(t, 1, gopherId)
+
+		// metrics assertion
+		expectedMetric := `
+			# HELP gophers_service_operations_total Number of operations on the GopherService
+			# TYPE gophers_service_operations_total counter
+			gophers_service_operations_total{operation="create"} 1
+		`
+
+		err = testutil.GatherAndCompare(
+			metricsRegistry,
+			strings.NewReader(expectedMetric),
+			"gophers_service_operations_total",
+		)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should fail", func(t *testing.T) {
+		// run test
+		internal.RunTest(t, fx.Populate(&db, &gopherService))
+
+		// close db
+		err := db.Close()
+		assert.NoError(t, err)
+
+		// result assertion
+		_, err = gopherService.Create(context.Background(), "", "")
+		assert.Error(t, err)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	var db *sql.DB
+	var gopherService *service.GopherService
+	var metricsRegistry *prometheus.Registry
+
+	t.Run("should succeed", func(t *testing.T) {
+		// reset
+		service.GopherServiceCounter.Reset()
+
+		// run test
+		internal.RunTest(
+			t,
+			fxsql.RunFxSQLSeeds(),
+			fx.Populate(&db, &gopherService, &metricsRegistry),
+		)
+
+		// result assertion
+		err := gopherService.Delete(context.Background(), 1)
+		assert.NoError(t, err)
+
+		// metrics assertion
+		expectedMetric := `
+			# HELP gophers_service_operations_total Number of operations on the GopherService
+			# TYPE gophers_service_operations_total counter
+			gophers_service_operations_total{operation="delete"} 1
+		`
+
+		err = testutil.GatherAndCompare(
+			metricsRegistry,
+			strings.NewReader(expectedMetric),
+			"gophers_service_operations_total",
+		)
+		assert.NoError(t, err)
+	})
+
+	t.Run("should fail", func(t *testing.T) {
+		// run test
+		internal.RunTest(t, fx.Populate(&db, &gopherService))
+
+		// close db
+		err := db.Close()
+		assert.NoError(t, err)
+
+		// result assertion
+		err = gopherService.Delete(context.Background(), 1)
+		assert.Error(t, err)
+	})
 }
